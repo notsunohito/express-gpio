1e35ad4bfda32f59c31d65ee9252f0ec918d0f1c
Shuichiro Kamiya <notsunohito@gmail.com>
2015-02-24 17:27:20 +0900

    Implement switches

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
+            title: 'Express GPIOs',
             message: err.message,
             error: err
         });
@@ -53,6 +54,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
+        title: 'Express GPIOs',
         message: err.message,
         error: {}
     });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,72 @@
-console.log('hello notsu');
+(function() {
+
+var switchesTemplate = _.template(
+          '<li id="switch<%= number %>">' +
+            '<span class="left"><%= name %></span>' +
+            '<div class="onoffswitch left">' +
+              '<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="gpio-switch<%= number %>">' +
+              '<label class="onoffswitch-label" for="gpio-switch<%= number %>">' +
+                '<span class="onoffswitch-inner"></span>' +
+                '<span class="onoffswitch-switch"></span>' +
+              '</label>' +
+            '</div>' +
+          '</li>');
+
+$("#pin8").on('click', toggleSwitch());
+$("#pin10").on('click', toggleSwitch());
+$("#pin12").on('click', toggleSwitch());
+$("#pin16").on('click', toggleSwitch());
+$("#pin18").on('click', toggleSwitch());
+$("#pin22").on('click', toggleSwitch());
+$("#pin24").on('click', toggleSwitch());
+$("#pin26").on('click', toggleSwitch());
+
+$("#pin3").on('click', toggleSwitch());
+$("#pin5").on('click', toggleSwitch());
+$("#pin7").on('click', toggleSwitch());
+$("#pin11").on('click', toggleSwitch());
+$("#pin13").on('click', toggleSwitch());
+$("#pin15").on('click', toggleSwitch());
+$("#pin19").on('click', toggleSwitch());
+$("#pin21").on('click', toggleSwitch());
+$("#pin23").on('click', toggleSwitch());
+
+
+function toggleSwitch(e) {
+    var exists = false;
+
+    return function(e) {
+        if(exists) {
+            exists = false;
+            e.currentTarget.classList.remove('gpio-pink');
+            e.currentTarget.classList.add('gpio-green');
+            removeSwitch(e);
+        } else {
+            exists = true;
+            e.currentTarget.classList.remove('gpio-green');
+            e.currentTarget.classList.add('gpio-pink');
+            appendSwitch(e);
+        }
+    };
+}
+
+function removeSwitch(e) {
+    var name = e.currentTarget.innerText;
+    var number = deriveNumber(name);
+    $('#switch' + number).remove();
+}
+
+function appendSwitch(e) {
+    var name = e.currentTarget.innerText;
+    var number = deriveNumber(name);
+    $("#switches").append(switchesTemplate({name: name, number: number}));
+}
+
+function deriveNumber(name) {
+    return name
+        .replace(/[^0-9]/g,'')
+        .replace(/^0/,'');
+}
+
+
+}).call(this);
